ea18b3a51e1baf39c2da1632c0b12470684ba536
Eka Prasetia <ekaone3033@gmail.com>
2020-10-04 12:12:49 +0700

    add note

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,21 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Covid19 - Next.js ID</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Selamat datang <a href="https://nextjs.org">Next.js Indonesia!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Halaman seputar informasi Covid19{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -56,10 +56,10 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
         </a>
       </footer>
     </div>
-  )
+  );
 }
